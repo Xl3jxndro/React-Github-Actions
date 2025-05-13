572f4670172159439c9c021be83c1cb3a2eaa1e1
Xl3jxndro <joale9809@gmail.com>
2025-05-12 19:29:02 -0500

    Tercer Commit [React-Github-Actions]

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import App from './App';
 
 describe('App', () => {
