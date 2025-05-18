5b2a8744ddafe4eb879730a58555eb227c1649aa
Xl3jxndro <joale9809@gmail.com>
2025-05-18 14:21:28 -0500

    Quinto Commit [React-Github-Actions]

--- src/logger.js
+++ src/logger.js
@@ -4,4 +4,23 @@ import log from 'loglevel';
 const isDev = typeof import.meta !== 'undefined' && import.meta.env && import.meta.env.DEV;
 log.setLevel(isDev ? 'debug' : 'warn');
 
+// Logging a archivo (solo en entorno Node, por ejemplo en CI)
+if (typeof process !== 'undefined' && process.release && process.release.name === 'node') {
+  try {
+    const fs = require('fs');
+    const logFile = './app.log';
+    const originalFactory = log.methodFactory;
+    log.methodFactory = function (methodName, logLevel, loggerName) {
+      const rawMethod = originalFactory(methodName, logLevel, loggerName);
+      return function (...args) {
+        fs.appendFileSync(logFile, `[${methodName.toUpperCase()}] ${args.join(' ')}\n`);
+        rawMethod(...args);
+      };
+    };
+    log.setLevel(log.getLevel());
+  } catch (e) {
+    // No hacer nada si require falla (por ejemplo, en navegador)
+  }
+}
+
 export default log;
